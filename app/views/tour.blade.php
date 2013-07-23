9670214ef031340997ef881df330505add07c7c1
Nick Ball <nicholaspball@gmail.com>
2013-07-22 21:40:41 -0500

    tour date reviews

--- app/views/tour.blade.php
+++ app/views/tour.blade.php
@@ -9,14 +9,40 @@
         <th>CITY</th>
     </thead>
     <tbody>
-        <tr>
+        <tr class="reviewed" data-review="review-1">
+            <td>09/28/13</td>
+            <td>Ann Arbor, MI</td>
+            <td>Canterbury House<span class="support">w/ The Ericksons</span></td>
+        </tr>
+
+        <tr class="reviewed" data-review="review-2">
             <td>09/28/13</td>
             <td>Ann Arbor, MI</td>
             <td>Canterbury House</td>
         </tr>
+
     </tbody>
 
 </table>
+
+<section class="tour-reviews">
+
+    <blockquote id="review-1" class="tour-review">
+        <a href="#">
+            <p>Her songs are delicate, contemplative folk...Standing up front, there were like-minded fans of this music and the band did a fine job of delivering smooth, involved patterns of thoughtful and focused folk music.</p>
+            <p class="source italic">- David Hintz, DC ROCK LIVE</p>
+        </a>
+    </blockquote>
+
+    <blockquote id="review-2" class="tour-review">
+        <a href="#">
+            <p>"Instantly, I am impressed with the instrumental choices and arrangements...there is always something cool going on."</p>
+            <p class="source italic">- DC ROCK LIVE</p>
+        </a>
+    </blockquote>
+
+</section>
+
 
 @stop
 
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,8 +1,23 @@
 $(function() {
 
+    //Nav Toggle
     $('.nav-toggle').on('click', function() {
         $("#nav").toggleClass('nav--open');
         $(this).toggleClass("nav-toggle--open");
+    });
+
+
+
+
+    //Tour Dates Review
+    $('.reviewed').on("click", function() {
+        var     review = "#" + $(this).attr("data-review"),
+                  $rowPosition = $(this).position();
+        $(this).toggleClass('active');
+        $(review).css({
+            right: "0",
+            top: $rowPosition.top + $(this).height()
+        }).slideToggle(100);
     });
 
 });
