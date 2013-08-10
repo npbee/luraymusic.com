cab416a7e81f1c00a8beed3775d4938652896737
Nick Ball <nicholaspball@gmail.com>
2013-08-10 15:24:14 -0500

    updated sentry seeder to add shannon user

--- app/database/seeds/SentrySeeder.php
+++ app/database/seeds/SentrySeeder.php
@@ -7,21 +7,33 @@ class SentrySeeder extends Seeder {
     public function run()
     {
 
+        // Sentry::getUserProvider()->create(array(
+        //     'email'       => 'nicholaspball@gmail.com',
+        //     'password'    => "admin",
+        //     'first_name'  => 'Nick',
+        //     'last_name'   => 'Ball',
+        //     'activated'   => 1,
+        // ));
+
         Sentry::getUserProvider()->create(array(
-            'email'       => 'nicholaspball@gmail.com',
-            'password'    => "admin",
-            'first_name'  => 'Nick',
-            'last_name'   => 'Ball',
+            'email'       => 'shacarey@gmail.com',
+            'password'    => "wilder1!",
+            'first_name'  => 'Shannon',
+            'last_name'   => 'Carey',
             'activated'   => 1,
         ));
 
-        Sentry::getGroupProvider()->create(array(
-            'name'        => 'Admin',
-            'permissions' => array('admin' => 1),
-        ));
+        // Sentry::getGroupProvider()->create(array(
+        //     'name'        => 'Admin',
+        //     'permissions' => array('admin' => 1),
+        // ));
 
         // Assign user permissions
-        $adminUser  = Sentry::getUserProvider()->findByLogin('nicholaspball@gmail.com');
+        // $adminUser  = Sentry::getUserProvider()->findByLogin('nicholaspball@gmail.com');
+        // $adminGroup = Sentry::getGroupProvider()->findByName('Admin');
+        // $adminUser->addGroup($adminGroup);
+
+        $adminUser  = Sentry::getUserProvider()->findByLogin('shacarey@gmail.com');
         $adminGroup = Sentry::getGroupProvider()->findByName('Admin');
         $adminUser->addGroup($adminGroup);
     }
