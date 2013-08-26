87057ca989d226c6bddc1693bbb17aa38207a9ab
Nick Ball <nicholaspball@gmail.com>
2013-08-25 20:10:34 -0500

    updated home page with wash post press link

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -6,19 +6,19 @@
 
     <blockquote class="album-quote album-quote--1">
         <p>The Wilder is an exquisite collection of post-bluegrass Americana and chamber folk that is both delicately refined and, thanks to the prominence of Carey's rich banjo backing, unpretentiously rustic.</p>
-        <p class="source italic"><a href="http://www.directcurrentmusic.com/music-news-new-music/luray-the-wilder.html">Direct Current Music</a></p>
+        <p class="source italic"><a target="_blank" href="http://www.directcurrentmusic.com/music-news-new-music/luray-the-wilder.html">Direct Current Music</a></p>
     </blockquote>
 
     <blockquote class="album-quote album-quote--2">
-        <p>Haunting, ephemeral, and just a little rustic...</p>
-        <p class="source italic"><a href="http://www.utne.com/utne-reader-music-sampler.aspx#axzz2Y1Xt33Gd">Utne Reader</a></p>
+        <p>...the standouts are Carey’s radiant voice, which fluctuates between soothing and soaring, and her melodic banjo strumming.</p>
+        <p class="source italic"><a target="_blank" href="http://www.washingtonpost.com/goingoutguide/luray-album-review-the-wilder/2013/08/22/5411a0fa-0687-11e3-9259-e2aafe5a5f84_story.html">The Washington Post</a></p>
     </blockquote>
 
     <figure>
         <img id="album-art" src="assets/images/album_artwork.jpg" alt="Luray album art">
         <figcaption>
             <h1>LURAY <span class="brand-color">THE WILDER</span></h1>
-            <p class="epsilon">Out 8/27/13 on iTunes</p>
+            <p class="epsilon">Out now on iTunes</p>
             <p class="epsilon">Produced by Sean Carey of Bon Iver</p>
         </figcaption>
     </figure>
