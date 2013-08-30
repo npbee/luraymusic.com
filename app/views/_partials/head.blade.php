a4c523818f15271c9af9e7228bc87f8651aefbc6
Nick Ball <nicholaspball@gmail.com>
2013-08-29 20:14:32 -0500

    styling and feature detection for <IE9

--- app/views/_partials/head.blade.php
+++ app/views/_partials/head.blade.php
@@ -5,5 +5,12 @@
     <title>Luray</title>
     <link href='http://fonts.googleapis.com/css?family=Josefin+Sans:400, 700,400italic' rel='stylesheet' type='text/css'>
     {{ HTML::style('assets/css/style.css') }}
+    <!--[if lt IE 9]>
+        {{ HTML::style('assets/css/ie.css') }}
+    <![endif]-->
+
+    <!--[if lt IE 9]>
+        <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
     <link rel="author" href="humans.txt">
 </head>
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -91,4 +91,14 @@ $(function() {
     });
 
 
+
+
+    //PNG FALLBACK
+    if(!Modernizr.svg) {
+        $('img[src*="svg"]').attr('src', function() {
+            return $(this).attr('src').replace('.svg', '.png');
+        });
+    }
+
+
 });
