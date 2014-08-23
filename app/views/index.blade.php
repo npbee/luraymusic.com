41f3c98e82c85633adfd8d7a6f4b2463d9a16e89
Nick Ball <nicholaspball@gmail.com>
2014-08-23 09:59:12 -0700

    remove quotes on home page

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -4,21 +4,20 @@
 
     <a href="https://soundcloud.com/luraymusic/sets/the-wilder-preview-mastered" class="sc-player"></a>
 
-    <blockquote class="album-quote album-quote--1">
+    <!--<blockquote class="album-quote album-quote--1">
         <p>...the standouts are Carey’s radiant voice, which fluctuates between soothing and soaring, and her melodic banjo strumming.</p>
         <p class="source italic"><a target="_blank" href="http://www.washingtonpost.com/goingoutguide/luray-album-review-the-wilder/2013/08/22/5411a0fa-0687-11e3-9259-e2aafe5a5f84_story.html">The Washington Post</a></p>
-    </blockquote>
+    </blockquote>-->
 
-    <blockquote class="album-quote album-quote--2">
+    <!--<blockquote class="album-quote album-quote--2">
         <p>...beautifully lush album.</p>
         <p class="source italic"><a target="_blank" href="http://www.npr.org/blogs/allsongs/2013/08/26/215800528/new-music-tv-on-the-radio-rokia-traore-lucius-more">NPR</a></p>
-    </blockquote>
+    </blockquote>-->
 
     <figure>
         <img id="album-art" src="assets/images/album_artwork.jpg" alt="Luray album art">
         <figcaption>
             <h1>LURAY <span class="brand-color">THE WILDER</span></h1>
-            <p class="epsilon">Out now on <a target="_blank" href="http://phobos.apple.com/WebObjects/MZStore.woa/wa/viewAlbum?id=662389920">iTunes</a></p>
             <p class="epsilon">Produced by Sean Carey of Bon Iver</p>
         </figcaption>
 
