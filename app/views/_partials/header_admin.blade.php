df0132971a81dd5cf61c4177b45df47e1308d17c
Nick Ball <nicholaspball@gmail.com>
2015-08-16 09:45:29 -0700

    fix admin nav

--- app/views/_partials/header_admin.blade.php
+++ app/views/_partials/header_admin.blade.php
@@ -1,19 +1,21 @@
 <header class="page-head" id="page-head">
 
-    <div class='page-head__contents'>
-        <a href="/" class="site-logo" id="site-logo">{{ HTML::image('assets/images/logo.svg') }}></a>
-        <a class="admin-logo" href="{{ URL::route('admin') }}">ADMIN</a>
-
-        <a id="nav-toggle" class="nav-toggle nav-toggle--closed"><span class="hide">MENU</span></a>
-
-        <ul class="nav site-nav" id="nav">
-            <li><a class="tour-nav--admin" href="{{ URL::route('admin.tour.index') }}">Tour Dates</a></li><!--
-            --><li><a class="images-nav--admin" href="{{ URL::route('admin.image.index') }}">IMAGES</a></li><!--
-            --><li><a class="video-nav--admin" href="{{ URL::route('admin.videos.index') }}">Videos</a></li><!--
-            --><li><a class="press-nav--admin" href="{{ URL::route('admin.press.index') }}">Press</a></li><!--
-            --><li><a class="albums-nav--admin" href="{{ URL::route('admin.albums.index') }}">Albums</a></li><!--
-            --><li><a class="logout-nav" href="{{ URL::route('admin.logout') }}">Logout</a>
-        </ul>
-    </div>
+    <nav class="admin-nav">
+        <div class="nav__left">
+            <a href="/" data-page="admin" class="site-logo" id="site-logo">{{ HTML::image('assets/images/logo.svg') }}</a><!--
+            --><a id="nav-toggle" class="nav-toggle nav-toggle--closed"></a>
+        </div><!--
+        
+        --><div class="nav__right">
+            <ul class="nav site-nav" id="nav">
+                <li><a class="tour-nav--admin" href="{{ URL::route('admin.tour.index') }}">Tour Dates</a></li><!--
+                --><li><a class="images-nav--admin" href="{{ URL::route('admin.image.index') }}">IMAGES</a></li><!--
+                --><li><a class="video-nav--admin" href="{{ URL::route('admin.videos.index') }}">Videos</a></li><!--
+                --><li><a class="press-nav--admin" href="{{ URL::route('admin.press.index') }}">Press</a></li><!--
+                --><li><a class="albums-nav--admin" href="{{ URL::route('admin.albums.index') }}">Albums</a></li><!--
+                --><li><a class="logout-nav" href="{{ URL::route('admin.logout') }}">Logout</a>
+            </ul>
+        </div>
+    </nav>
 
 </header>
