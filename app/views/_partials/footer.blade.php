522ef53a749838fef04b2a106f63c639c6619a01
Nick Ball <nicholaspball@gmail.com>
2015-08-16 09:23:02 -0700

    redo contact apge

--- app/views/_partials/footer.blade.php
+++ app/views/_partials/footer.blade.php
@@ -38,13 +38,13 @@
         --><section class="footer__right">
             <div class="mailing-list">
                 <p class="footer__header">Mailing List</p>
-                <form class="input-combo" action="http://luraymusic.us5.list-manage.com/subscribe/post?u=8da68ebf6c5070fcc364c1658&amp;id=64e96e37b2" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                <form action="http://luraymusic.us5.list-manage.com/subscribe/post?u=8da68ebf6c5070fcc364c1658&amp;id=64e96e37b2" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="input-combo validate" target="_blank" novalidate>
                     <label for="mce-EMAIL"></label>
                     <input type="email" value="" name="EMAIL" class="text-input email" id="mce-EMAIL" placeholder="email address" required><!--
                     --><input type="submit" value="submit" name="subscribe" id="mc-embedded-subscribe" class="button submit">
                 </form>
             </div>
 
-        </section><!--
+        </section>
     </div>
 </footer>
--- app/views/contact.blade.php
+++ app/views/contact.blade.php
@@ -2,40 +2,48 @@
 
 @section('content')
 
-<div class="contact-block-big">
+<div class="contact-block">
 
         <section>
             <div class="booking">
-                <h1>Booking</h1>
-                <p><a href="mailto:luray@luraymusic.com">luray@luraymusic.com</a></p>
+                <h2 class="contact-block__header">Booking</h1>
+                <p class="contact-block__content"><a href="mailto:luray@luraymusic.com">luray@luraymusic.com</a></p>
             </div>
+        </section><!--
 
+        --><section>
             <div class="publicity">
-                <h1>Publicity</h1>
-                <p><a href="http://www.thinkpress.net/index.php/clients/luray">Monica Hopman</a></p>
-                <p><a href="mailto:monica@thinkpress.net">monica@thinkpress.net</a></p>
+                <h2 class="contact-block__header">Publicity</h2>
+                <div class="contact-block__content">
+                    <p><a href="http://www.thinkpress.net/index.php/clients/luray">Monica Hopman</a></p>
+                    <p><a href="mailto:monica@thinkpress.net">monica@thinkpress.net</a></p>
+                </div>
             </div>
+        </section><!--
 
+        --><section>
             <div class="radio">
-                <h1>Radio</h1>
+                <h2 class="contact-block__header">Radio</h2>
                 <p>Paul Brown, Red Hat 22</p>
                 <p><a href="mailto:redhat22@mac.com">redhat22@mac.com</a></p>
             </div>
 
-        </section>
+        </section><!--
 
-        <section>
+        --><section>
             <div class="mailing-list">
-                <h1>Mailing list</h1>
-                <form action="http://luraymusic.us5.list-manage.com/subscribe/post?u=8da68ebf6c5070fcc364c1658&amp;id=64e96e37b2" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                <h2 class="contact-block__header">Mailing list</h2>
+                <form action="http://luraymusic.us5.list-manage.com/subscribe/post?u=8da68ebf6c5070fcc364c1658&amp;id=64e96e37b2" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate input-combo" target="_blank" novalidate>
                     <label for="mce-EMAIL"></label>
-                    <input type="email" value="" name="EMAIL" class="text-input email" id="mce-EMAIL" placeholder="email address" required>
-                    <input type="submit" value="submit" name="subscribe" id="mc-embedded-subscribe" class="button submit">
+                    <input type="email" value="" name="EMAIL" class="text-input email" id="mce-EMAIL" placeholder="email address" required><!--
+                    --><input type="submit" value="submit" name="subscribe" id="mc-embedded-subscribe" class="button submit">
                 </form>
             </div>
+        </section><!--
 
+        --><section>
             <div class="social">
-                <h1>Social</h1>
+                <h2 class="contact-block__header">Social</h2>
                 <p>
                     <a class="facebook tooltip" title="facebook" data-icon="&#xe00e;" href="https://www.facebook.com/Luraymusic" target="_blank"><span class="hide">Facebook</span></a>
                     <a class="twitter" title="twitter" data-icon="&#xe00c;" href="https://twitter.com/luray_who" target="_blank"><span class="hide">Twitter</span></a>
@@ -51,4 +59,8 @@
 
 
 
+@stop
+
+@section('footer')
+    @include('_partials.footer')
 @stop
