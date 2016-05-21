9a57fa17bd30e0dd0ba43515413940ceda23dc85
Nick Ball <nicholaspball@gmail.com>
2016-05-21 12:53:00 -0700

    add tumblr api calls to home page

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -3,30 +3,10 @@
 @section('content')
 
     <a href="https://soundcloud.com/luraymusic/sets/the-wilder-preview-mastered" class="sc-player"></a>
-
-    <!--<blockquote class="album-quote album-quote--1">
-        <p>...the standouts are Carey’s radiant voice, which fluctuates between soothing and soaring, and her melodic banjo strumming.</p>
-        <p class="source italic"><a target="_blank" href="http://www.washingtonpost.com/goingoutguide/luray-album-review-the-wilder/2013/08/22/5411a0fa-0687-11e3-9259-e2aafe5a5f84_story.html">The Washington Post</a></p>
-    </blockquote>-->
-
-    <!--<blockquote class="album-quote album-quote--2">
-        <p>...beautifully lush album.</p>
-        <p class="source italic"><a target="_blank" href="http://www.npr.org/blogs/allsongs/2013/08/26/215800528/new-music-tv-on-the-radio-rokia-traore-lucius-more">NPR</a></p>
-    </blockquote>-->
-
-    <figure>
-        <img id="album-art" src="assets/images/album_artwork.jpg" alt="Luray album art">
-        <figcaption>
-            <h1>LURAY <span class="brand-color">THE WILDER</span></h1>
-            <p class="epsilon">Produced by Sean Carey of Bon Iver</p>
-        </figcaption>
-
-    </figure>
-
-    <ul class="purchase">
-        <li><a class="itunes" href="http://phobos.apple.com/WebObjects/MZStore.woa/wa/viewAlbum?id=662389920" target="_blank">{{ HTML::image('assets/images/logos/itunes.png', 'itunes logo')}}</a></li>
-        <li><a target="_blank" class="bigcartel" href="http://luray.bigcartel.com/product/the-wilder-cd">{{ HTML::image('assets/images/logos/bigcartel.png', 'bigcartel logo')}}</a></li>
-        <!-- <li><a class="amazon" href="#">{{ HTML::image('assets/images/logos/amazon.png', 'amazon logo')}}</a></li> -->
-            </ul>
+    <div id="tumblr" class="tumblr"></div>
+    <div id="loader" class="loader" style="display: none">Loading...</div>
+    <div class="load-more">
+        <button id="load-more" style="display: none" class="button">Load more posts</button>
+    </div>
 
 @stop
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,5 +1,174 @@
 $(function() {
 
+    // Tumblr
+    var apiKey = 'qgvww1o5nwzS3Fyv8wnKXrMizEApuugmlZdeJvZHdh4M5yhLwd';
+    var apiBase = 'http://api.tumblr.com/v2/blog/luraymusic.tumblr.com/posts?' +
+        'api_key=' + apiKey;
+    var limit = 5;
+    var currentOffset = 0;
+    var OFFSET_INC = 5;
+    var tumblr = $('#tumblr');
+    var loader = $('#loader');
+    var loadMore = $('#load-more');
+    var handledTypes = ['photo', 'video', 'text'];
+
+    function renderDate(post) {
+        var date = new Date(post.date);
+        var month = date.getMonth() + 1;
+        var day = date.getDate();
+        var year = date.getFullYear();
+        var $date = $('<div/>', {
+            class: 'post__date',
+            text: month + '/' + day + '/' + year
+        });
+
+        return $date;
+    }
+
+    function renderLink(post) {
+        var permalink = post.post_url;
+
+        var $link = $('<a/>', {
+            href: permalink,
+            class: 'post__link',
+            text: 'link'
+        });
+
+        return $link;
+    }
+
+    function renderPhotoPost(post) {
+        var $photosContainer = $('<div/>', { class: 'photo-post' });
+        var $caption = $('<div/>', {
+            class: 'post__caption',
+            html: post.caption
+        });
+        var $photos = post.photos.map(function(photo) {
+            var finalUrl;
+            var originalSize = photo.original_size.url;
+
+            // First try to grab the 500 width photo
+            var mediumSize = photo.alt_sizes.filter(function(photo) {
+                return photo.width === 500;
+            });
+
+            if (mediumSize.length) {
+                mediumSize = mediumSize[0].url;
+            } else {
+                mediumSize = null;
+            }
+
+            finalUrl = mediumSize || originalSize;
+
+            return $('<img/>', { src: finalUrl });
+        });
+
+        $photosContainer.append($photos).append($caption);
+
+        return $photosContainer;
+    }
+
+    function renderVideoPost(post) {
+        var $container = $('<div/>', { class: 'video-post' });
+        var $caption = $('<div/>', {
+            class: 'post_caption',
+            html: post.caption
+        });
+        var player = post.player.filter(function(player) {
+            return player.width === 500;
+        });
+
+        var $player = $('<div/>', {
+            class: 'post_player',
+            html: player[0].embed_code
+        });
+
+        $container.append($player).append($caption);
+
+        return $container;
+    }
+
+    function renderTextPost(post) {
+        var $container = $('<div/>', { class: 'post_text' });
+        var $body = $('<div/>', {
+            class: 'post_body',
+            html: post.body
+        });
+
+        $container.append($body);
+
+        return $container;
+    }
+
+    function renderPost(post) {
+        var $post = $('<div/>', {
+            class: 'post'
+        });
+
+        var $date = renderDate(post);
+        $post.append($date);
+
+        var $content;
+
+        if (post.type === 'photo') {
+            $content = renderPhotoPost(post);
+        } else if (post.type === 'video') {
+            $content = renderVideoPost(post);
+        } else if (post.type === 'text') {
+            $content = renderTextPost(post);
+        }
+
+        $post.append($content);
+
+        var $link = renderLink(post);
+        $post.append($link);
+
+        return $post;
+    }
+
+    function getPosts() {
+
+        loader.show();
+
+        var url = apiBase +
+            '&limit=' + limit +
+            '&offset=' + currentOffset;
+
+        $.ajax({
+            url: url,
+            dataType: 'jsonp'
+        })
+        .then(function(data) {
+            loader.hide();
+            loadMore.show();
+            var posts = data.response.posts;
+            var renderedPosts = posts
+                .filter(function(post) {
+                    return handledTypes.indexOf(post.type) !== -1;
+                }).map(renderPost);
+            renderedPosts.forEach(function(renderedPost) {
+                tumblr.append(renderedPost);
+            });
+        })
+        .fail(function() {
+            tumblr.html('' +
+                '<p>There was a problem fetching the Tumblr posts!</p>' +
+                '<p>Please try visiting the Tumblr directly at this location: ' +
+                    '<a href="http://news.luraymusic.com">news.luraymusic.com</a></p>');
+        });
+    }
+
+    // only on home page
+    if (tumblr.length) {
+        getPosts();
+
+        loadMore.on('click', function(e) {
+            currentOffset += OFFSET_INC;
+
+            return getPosts();
+        });
+    }
+
     //Nav Toggle
     $('.nav-toggle').on('click', function() {
         $("#nav").toggleClass('nav--open');
