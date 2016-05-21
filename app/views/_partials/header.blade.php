e976f3bf22679850048cf74b6b8898918909731d
Nick Ball <nicholaspball@gmail.com>
2016-05-21 12:53:56 -0700

    remove news links

--- app/views/_partials/header.blade.php
+++ app/views/_partials/header.blade.php
@@ -8,8 +8,7 @@
 
         --><div class="nav__right">
             <ul class="nav site-nav" id="nav">
-                <li><a class="news-nav" href="http://news.luraymusic.com">News</a></li><!--
-                --><li><a class="media-nav" href="{{ URL::route('media') }}">Media</a></li><!--
+                <li><a class="media-nav" href="{{ URL::route('media') }}">Media</a></li><!--
                 --><li><a class="albums-nav" href="{{ URL::route('albums') }}">Albums</a></li><!--
                 --><li><a class="tour-nav" href="{{ URL::route('tour') }}">Tour</a></li><!--
                 --><li><a class="contact-nav" href="{{ URL::route('contact') }}">Contact</a></li>
